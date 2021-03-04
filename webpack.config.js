b99c64bef1f0f1c2ba6347f2ad6f9886e85a2a9f
ProlNatali <boltacheva.natalya@mail.ru>
2021-03-05 02:55:25 +0300

    update webpack(outputPath)

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,7 @@ module.exports = {
 						loader: 'file-loader',
 						options: {
 							name: '[name].[ext]',
-							outputPath: 'css/fonts/Roboto'
+							outputPath: '../css/fonts/Roboto'
 						}
 					}
 				]
