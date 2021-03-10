725b3d3d0896894a3d0d907217ed98ea7827c59b
ProlNatali <boltacheva.natalya@mail.ru>
2021-03-10 15:37:51 +0300

    deleted unnecessary files

--- README.md
+++
@@ -1,1 +0,0 @@
-# 2sort-animation
--- js/redux/index.js
+++
@@ -1,6 +0,0 @@
-import {combineReducers} from 'redux'
-import {reducer as reducerArray} from './reducerArray.js';
-
-export default combineReducers({
-	reducerArray
-})
